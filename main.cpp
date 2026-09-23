7e69043c84839e8c44ffe6cde7d83b89c0c1ea89
Вероніка <veronika@MacBook-Air-Veronika.local>
2026-09-23 23:01:57 +0300

    Перший коміт

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double alpha; // вхідний параметр
+    double z1;    // результат обчислення першої формули
+    double z2;    // результат обчислення другої формули
+
+    const double pi = 4 * atan(1.0);
+
+    cout << "alpha = ";
+    cin >> alpha;
+
+    z1 = sin(pi / 2 + 3 * alpha) /
+         (1 - sin(3 * alpha - pi));
+
+    z2 = 1 / tan(5 * pi / 4 + 3 * alpha / 2);
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0;
+}
